a3e5e517c86c8abe94a1573be2fafe39c4add538
DoctaDocta <trashm96@gmail.com>
2015-09-22 22:05:33 -0500

    added fileFound = False

--- analyze.py
+++ analyze.py
@@ -34,18 +34,19 @@ print "\nReading in meninFile. Saving double to DB.",'\n',"-"*63
 with open (meninFile, 'rb') as f:
     reader = csv.reader(f, delimiter="\t")
     for line in reader:
-            print 'line:', line
-            #searching to see if this already exists.
-            for x in cabinet:
-                if (x.name == line[2][:12]):
-                    print "\tFound matching tissue sample name!"
-                    x.add_tissue_expression(line[3], line[1])
-                    print "\tAdding expressionvalue to sample.\n"
-                    fileFound = True;
-            if (fileFound == False):
-                print '\ttemporarily adding new sample to cabinet.\n'
-                x = Sample(line[2],line[0],line[3],line[1])
-                cabinet.append(x)
+        fileFound = False;
+        print 'line:', line
+        #searching to see if this already exists.
+        for x in cabinet:
+            if (x.name == line[2][:12]):
+                print "\tFound matching tissue sample name!"
+                x.add_tissue_expression(line[3], line[1])
+                print "\tAdding expressionvalue to sample.\n"
+                fileFound = True;
+        if (fileFound == False):
+            print '\ttemporarily adding new sample to cabinet.\n'
+            x = Sample(line[2],line[0],line[3],line[1])
+            cabinet.append(x)
 
 print '\n',"-"*63, '\n',"Now executing the cabinet to table, 'tissue', in database file."
 print "Only executing Sampels with both tissue types.",'\n',"-"*63
